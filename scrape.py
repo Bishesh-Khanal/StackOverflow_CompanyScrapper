b76d3ed46011b353213a02f6c798b37d0d61bed8
Bishesh Khanal <bishesh.khanal25@gmail.com>
2023-09-30 22:03:04 +0545

    Created user agents

--- scrape.py
+++ scrape.py
@@ -1,6 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import random
+
+userAgents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.3',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.3',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.40',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.31',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.3',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.43',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+    ]
 
 companies_dictionary = {'Name': [], 'Location': [], 'Type': [], 'Website': [], 'Specialities': [], 'Size': [], 'Founded': [], 'Status': [], 'Followers on Stack Overflow':[]}
 
@@ -24,7 +45,7 @@ def scrape(companies):
         companies_dictionary['Specialities'].append(specialities_text)
         
         link = company.find('a')['href']
-        company_text = requests.get('https://stackoverflow.com' + link).text
+        company_text = requests.get('https://stackoverflow.com' + link, headers = random.choice(userAgents)).text
         soup = BeautifulSoup(company_text, 'lxml')
         
         additional = soup.find('div', class_ = 'ba bc-black-100 ps-relative p16 bar-sm')
@@ -63,7 +84,7 @@ def scrape(companies):
         companies_dictionary['Followers on Stack Overflow'].append(followers)
 
 for page in range (1, 12):
-    html_text = requests.get('https://stackoverflow.com/jobs/companies?pg=' + str(page)).text
+    html_text = requests.get('https://stackoverflow.com/jobs/companies?pg=' + str(page), headers = random.choice(userAgents)).text
     soup = BeautifulSoup(html_text, 'lxml')
 
     companies = soup.find_all('div', class_ = 'flex--item fl1 text mb0')
